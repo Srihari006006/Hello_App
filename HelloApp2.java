fabea1733fb67f82f067c870902ac026699495d3
Srihari006006 <haripalanikumar2020@gmail.com>
2026-03-17 08:15:39 +0530

    Added HelloApp2

---
+++ HelloApp2.java
@@ -0,0 +1,13 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        
+        // Check if user has provided any argument
+        if (args.length > 0) {
+            String name = args[0];   // Get first argument
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no argument is given
+            System.out.println("Hello, World!");
+        }
+    }
+}
