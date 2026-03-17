492c4ab979d98993a27fa9b950009570f953325b
Srihari006006 <haripalanikumar2020@gmail.com>
2026-03-17 08:31:09 +0530

    Implemented UC2: Display name using command-line argument

--- HelloApp2.java
+++ HelloApp2.java
@@ -1,12 +1,12 @@
 public class HelloApp2 {
     public static void main(String[] args) {
         
-        // Check if user has provided any argument
+        
         if (args.length > 0) {
-            String name = args[0];   // Get first argument
+            String name = args[0];   
             System.out.println("Hello, " + name + "!");
         } else {
-            // Default message if no argument is given
+            
             System.out.println("Hello, World!");
         }
     }
